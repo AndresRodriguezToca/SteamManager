b387469dd609f89c210823d44edde9eb3505c71c
Andres Rodriguez Toca <48394901+AndresRodriguezToca@users.noreply.github.com>
2023-06-11 13:53:26 -0400

    SteamOwnedGames Implemented, Changes to UI

--- Services/SteamWebAPI.cs
+++ Services/SteamWebAPI.cs
@@ -12,6 +12,7 @@ namespace SteamManager.Services
     using System.Security.Cryptography;
     using System.Text;
     using System.Threading.Tasks;
+    using System.Windows;
     using System.Xml.Linq;
     using Newtonsoft.Json;
     using SteamManager.Class;
@@ -24,6 +25,7 @@ namespace SteamManager.Services
         private const string iSteamUser = "iSteamUser"; //Steam provides API calls to provide information about Steam users.
         private const string ISteamNews = "ISteamNews"; //Steam provides methods to fetch news feeds for each Steam game.
         private const string ISteamUserStats = "ISteamUserStats"; //Steam provides methods to fetch global stat information by game.
+        private const string IPlayerService = "IPlayerService"; //Provides additional methods for interacting with Steam Users.
 
         //ENCRYPTER (IN CASE I NEEDED FOR STEAM)
         private Encrypter Encrypter = new Encrypter();
@@ -31,26 +33,29 @@ namespace SteamManager.Services
 
         private readonly HttpClient httpClient;
         private readonly string apiKey;
+        private readonly string steamID;
+        SteamOwnedGames steamOwnedGames;
 
-        public SteamApiClient(string apiKey)
+        public SteamApiClient(string apiKey, string steamID)
         {
             this.apiKey = apiKey;
+            this.steamID = steamID;
             this.httpClient = new HttpClient();
         }
 
-        public async Task<SteamAccount> ValidateUserAPI(string sdk, string steamID)
+        public async Task<SteamAccount> ValidateUserAPI()
         {
 
             try
             {
                 // Send a POST request to the Steam API for user authentication
-                var request = new HttpRequestMessage(HttpMethod.Get, $"{SteamApiBaseUrl}/{iSteamUser}/GetPlayerSummaries/v0002/?key={sdk}&steamids={steamID}");
+                var request = new HttpRequestMessage(HttpMethod.Get, $"{SteamApiBaseUrl}/{iSteamUser}/GetPlayerSummaries/v0002/?key={this.apiKey}&steamids={this.steamID}");
 
                 var response = await httpClient.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
                 Console.WriteLine(response);
                 var responseContent = await response.Content.ReadAsStringAsync();
 
-                if(responseContent != null)
+                if (responseContent != null)
                 {
                     SteamAccount steamAccount = new SteamAccount(responseContent);
                     return steamAccount;
@@ -67,16 +72,30 @@ namespace SteamManager.Services
             }
         }
 
-        private string ParseSteamIdFromResponse(string response)
+        public async Task<SteamOwnedGames> GetOwnerGames(SteamAccount steamAccount)
         {
-            // Parse the response JSON to extract the SteamID
-            // Replace this with your own JSON parsing logic to extract the SteamID
-            // The response JSON structure will depend on the specific Steam API endpoint you are using
-            // Consult the Steam Web API documentation for the appropriate response structure
-            // Here's an example assuming the response contains a field named "steamid"
-            var steamId = "1234567890"; // Replace with your parsing logic
+            try
+            {
+                // Send a POST request to the Steam API for user authentication
+                var request = new HttpRequestMessage(HttpMethod.Get, $"{SteamApiBaseUrl}/{IPlayerService}/GetOwnedGames/v1/?key={this.apiKey}&steamid={this.steamID}&include_appinfo=true&include_played_free_games=true");
+                var response = await httpClient.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
+                var responseContent = await response.Content.ReadAsStringAsync();
 
-            return steamId;
+                if (responseContent != null)
+                {
+                    SteamOwnedGames steamOwnedGames = new SteamOwnedGames(responseContent);
+                    return steamOwnedGames;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception)
+            {
+                return null;
+            }
         }
+
     }
 }
