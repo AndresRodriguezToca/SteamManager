d37bec60f7937a747f85699027eebc10a58c897b
Andres Rodriguez Toca <48394901+AndresRodriguezToca@users.noreply.github.com>
2024-06-25 21:18:34 -0400

    Added a little profile picture to every game (Owner's Picture)

--- templates/snippets/games_collection.php
+++ templates/snippets/games_collection.php
@@ -8,6 +8,7 @@
 		<?php
 			// INITIATE CLASS
 			$simplyHeader = new Modules\Header("Steam Manager - Games Collection");
+            $simplyHeader->includeAdditionalCSS($GLOBALS["webroot"] . "/library/css/games_collection.css");
 			$simplyHeader->_includeSimplyMeta();
 			$simplyHeader->_includeSimplyCSS();
 			$simplyHeader->_includeGlobalsCSS();
@@ -16,67 +17,71 @@
     <body>
 		<div class="container">
 			<div class="row">
-			<?php
-// DISPLAY A PICTURE OF EVERY ACCOUNT AND THEIR GAMES
-$folderPath = __DIR__ . '/../../accounts/';
-$secondaryAccounts = json_decode($_ENV["secondary_accounts"], true);
-$mainAccount = $_ENV["main_account"];
-array_unshift($secondaryAccounts, $mainAccount);
+			    <?php
+                    // DISPLAY A PICTURE OF EVERY ACCOUNT AND THEIR GAMES
+                    $folderPath = __DIR__ . '/../../accounts/';
+                    $secondaryAccounts = json_decode($_ENV["secondary_accounts"], true);
+                    $mainAccount = $_ENV["main_account"];
+                    array_unshift($secondaryAccounts, $mainAccount);
 
-$processedGames = [];
-$gamesToDisplay = [];
+                    $processedGames = [];
+                    $gamesToDisplay = [];
 
-// Collect game data
-foreach ($secondaryAccounts as $account) {
-    $accountData = json_decode(file_get_contents($folderPath . $account . '/games.json'), true);
-    $games = $accountData['response']['games'];
+                    // COMPILE GAME DATA
+                    foreach ($secondaryAccounts as $account) {
+                        // FETCH ACCOUNT PICTURE
+                        $accountInformation = json_decode(file_get_contents($folderPath . $account . '/account.json'), true);
+                        $avatarUrl = $accountInformation['response']['players'][0]['avatarfull'];
+                        // FETCH ACCOUNT GAMES
+                        $accountData = json_decode(file_get_contents($folderPath . $account . '/games.json'), true);
+                        $games = $accountData['response']['games'];
+                        foreach ($games as $game) {
+                            if (in_array($game['appid'], $processedGames)) {
+                                continue;
+                            }
 
-    foreach ($games as $game) {
-        if (in_array($game['appid'], $processedGames)) {
-            continue;
-        }
+                            $gameData = null;
+                            $gameFilePath = __DIR__ . '/../../library/games_data/' . $game['appid'] . '.json';
 
-        $gameData = null;
-        $gameFilePath = __DIR__ . '/../../library/games_data/' . $game['appid'] . '.json';
+                            if (file_exists($gameFilePath)) {
+                                $gameData = json_decode(file_get_contents($gameFilePath), true);
+                            } else {
+                                $gameData = json_decode(file_get_contents('https://store.steampowered.com/api/appdetails?appids=' . $game['appid']), true);
+                                file_put_contents($gameFilePath, json_encode($gameData));
+                            }
 
-        if (file_exists($gameFilePath)) {
-            $gameData = json_decode(file_get_contents($gameFilePath), true);
-        } else {
-            $gameData = json_decode(file_get_contents('https://store.steampowered.com/api/appdetails?appids=' . $game['appid']), true);
-            file_put_contents($gameFilePath, json_encode($gameData));
-        }
+                            if (isset($gameData) && $gameData[$game['appid']]['success']) {
+                                $gameName = $gameData[$game['appid']]['data']['name'];
+                                $gameImage = $gameData[$game['appid']]['data']['header_image'];
+                                $processedGames[] = $game['appid'];
 
-        if (isset($gameData) && $gameData[$game['appid']]['success']) {
-            $gameName = $gameData[$game['appid']]['data']['name'];
-            $gameImage = $gameData[$game['appid']]['data']['header_image'];
-            $processedGames[] = $game['appid'];
+                                // STORE GAME INFO
+                                $gamesToDisplay[] = [
+                                    'name' => $gameName,
+                                    'image' => $gameImage,
+                                    'owner_profile_picture' => $avatarUrl
+                                ];
+                            }
+                        }
+                    }
 
-            // Store game info for sorting
-            $gamesToDisplay[] = [
-                'name' => $gameName,
-                'image' => $gameImage
-            ];
-        }
-    }
-}
+                    // SORT GAME LIST ALPHABETICALLY
+                    usort($gamesToDisplay, function($a, $b) {
+                        return strcmp($a['name'], $b['name']);
+                    });
 
-// Sort games alphabetically by name
-usort($gamesToDisplay, function($a, $b) {
-    return strcmp($a['name'], $b['name']);
-});
-
-// Display sorted games
-foreach ($gamesToDisplay as $game) {
-    ?>
-    <div class="col-2 m-2">
-        <div class="game-wrapper js-glare-scale" data-tippy-content="<?php echo htmlspecialchars($game['name']) ?>">
-            <img src="<?php echo htmlspecialchars($game['image']) ?>" class="img-fluid rounded mx-auto d-block">
-        </div>
-    </div>
-    <?php
-}
-?>
-
+                    // DISPLAY SORTED GAMES
+                    foreach ($gamesToDisplay as $game) {
+                        ?>
+                        <div class="col-3 mt-5 m-2">
+                            <div data-tilt class="game-wrapper js-glare-scale tilt-parent" data-tippy-content="<?php echo htmlspecialchars($game['name']) ?>">
+                                <img src="<?php echo $game['owner_profile_picture'] ?>" class="avatar-style tilt-child avatar-game-owner rounded">
+                                <img src="<?php echo htmlspecialchars($game['image']) ?>" class="img-fluid rounded mx-auto d-block">
+                            </div>
+                        </div>
+                        <?php
+                    }
+                ?>
 			</div>
 		</div>
     </body>
